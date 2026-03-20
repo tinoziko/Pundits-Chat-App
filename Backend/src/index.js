56f0a90e2a5632da638ccc56992d3e6bb86a1d7e
Tinotenda Ziko <179060330+tinoziko@users.noreply.github.com>
2026-03-20 18:44:02 +0100

    Update to index.js - Missing parenthesis

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -27,7 +27,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(_dirname, "../Frontend/dist"));
+    app.use(express.static(path.join(_dirname, "../Frontend/dist")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.join(_dirname, "../Frontend", "dist", "index.html"));
