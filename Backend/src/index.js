57824894d6ef0dc1e649a22e73b7b957ee80adf7
Tinotenda Ziko <179060330+tinoziko@users.noreply.github.com>
2026-03-20 08:14:44 +0100

    Update index.js with the new content

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -26,8 +26,8 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(_dirname, "../Frontend/dist"));
+if(process.env.NODE_ENV=== "production"){
+    app.use(express.static(path.join(_dirname, "../Frontend/dist"))); 
 
     app.get("*", (req, res) => {
         res.sendFile(path.join(_dirname, "../Frontend", "dist", "index.html"));
